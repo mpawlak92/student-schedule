bf0da4c569f587b5571155f4c0943f0fa3ccd136
Michal Pawlak <mpawlak.it@gmail.com>
2023-10-29 00:37:03 +0200

    lock screen orientation in portrait mode

--- src/app/start/start.page.ts
+++ src/app/start/start.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ScreenOrientation } from '@capacitor/screen-orientation';  
 @Component({
   selector: 'app-start',
   templateUrl: './start.page.html',
@@ -10,6 +10,9 @@ export class StartPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    ScreenOrientation.lock({
+      orientation: 'portrait', // or 'landscape'
+    });
   }
 
 }
