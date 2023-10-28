bc342336a96ba7ea2f3c88cbc245b8a85fe3d288
Michal Pawlak <mpawlak.it@gmail.com>
2023-10-28 20:36:06 +0200

    feat: sign-in page with login and validation logic

--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,13 +6,68 @@ import { Router } from '@angular/router';
   templateUrl: './sign-in.page.html',
   styleUrls: ['./sign-in.page.scss'],
 })
-export class SignInPage implements OnInit {
+export class SignInPage  {
+emailValue = '';
+  passwordValue = '';
+  emailValidationResult = '';
+  passwordValidationResult = '';
 
-  constructor(private router:Router) { }
+  loginBtn = true;
+  constructor(public router: Router) {}
 
-  ngOnInit() {
+  validateEmail(email: string): string | null {
+    // You can use a regular expression to validate the email format
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailPattern.test(email)) {
+      return 'Invalid email format';
+    }
+
+    return null; // Email is valid
   }
-  login(){
-    this.router.navigateByUrl('/home')
+
+  validatePassword(password: string): string | null {
+    // Password must be at least 6 characters long
+    if (password.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+
+    // Password must contain at least one special character
+    const specialCharacterPattern = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+    if (!specialCharacterPattern.test(password)) {
+      return 'Password must contain at least one special character';
+    }
+
+    // Password must contain at least one capital letter
+    const capitalLetterPattern = /[A-Z]/;
+    if (!capitalLetterPattern.test(password)) {
+      return 'Password must contain at least one capital letter';
+    }
+
+    return null; // Password is valid
+  }
+  login() {
+    this.emailValidationResult = '';
+    this.passwordValidationResult = '';
+    const emailValidationResult = this.validateEmail(this.emailValue);
+    const passwordValidationResult = this.validatePassword(this.passwordValue);
+
+    if (emailValidationResult) {
+      this.emailValidationResult = emailValidationResult;
+      this.passwordValue = '';
+    } else if (passwordValidationResult) {
+      this.passwordValidationResult = passwordValidationResult;
+      this.passwordValue = '';
+    } else {
+      this.emailValidationResult = '';
+      this.passwordValidationResult = '';
+      this.inputsClear();
+      this.router.navigateByUrl('home');
+    }
+    this.router.navigateByUrl('home'); //for development
+  }
+  inputsClear() {
+    this.emailValue = '';
+    this.passwordValue = '';
   }
 }
