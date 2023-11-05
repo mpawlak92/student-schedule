b3df4c9b30dc18795a8147b09bebc7de8e1852cd
Michal Pawlak <mpawlak.it@gmail.com>
2023-11-05 21:49:59 +0100

    added loading spiner template

---
+++ src/app/loading-spiner/loading-spiner.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spiner',
+  templateUrl: './loading-spiner.component.html',
+  styleUrls: ['./loading-spiner.component.scss'],
+  standalone:true
+  
+})
+export class LoadingSpinerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
