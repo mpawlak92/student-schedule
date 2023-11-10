ae7da90435b385d95fc19500d7263726c8ba11ea
Michal Pawlak <mpawlak.it@gmail.com>
2023-11-10 20:10:43 +0100

    create questionnaire for evaluation modal

--- src/app/classes-modal/class-evaluation-modal/class-evaluation-modal.component.ts
+++ src/app/classes-modal/class-evaluation-modal/class-evaluation-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -8,9 +9,62 @@ import { ModalController } from '@ionic/angular';
 })
 export class ClassEvaluationModalComponent  implements OnInit {
 
-  constructor(private modalCtrl: ModalController) {}
+questions = [
+  'Jak oceniasz ogólną jakość prowadzenia zajęć na uniwersytecie?',
+  'Czy materiały dydaktyczne dostarczane na zajęciach są zrozumiałe i pomocne?',
+  'Jak oceniasz interakcję z wykładowcami i innymi studentami podczas zajęć?',
+  'Czy oferta przedmiotów jest zróżnicowana i odpowiada Twoim oczekiwaniom?',
+  'Czy uczestnictwo w zajęciach przyczynia się do Twojego rozwoju zawodowego i osobistego?'
+  ];
 
-  ngOnInit() {}
+  stars = [1, 2, 3, 4, 5];
+  hoveredValue = [0, 0, 0, 0, 0];
+  ratings = [0, 0, 0, 0, 0];
+
+  questionnaireForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private modalCtrl:ModalController) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+  const formControls = this.questions.map((question, index) => {
+    return this.fb.control(null, [Validators.required, this.ratingValidator]);
+  });
+
+  this.questionnaireForm = this.fb.group({
+    ratings: this.fb.array(formControls),
+  });
+}
+
+ratingValidator(control: AbstractControl): ValidationErrors | null {
+  const value = control.value;
+  return value !== null && value !== 0 ? null : { 'required': true };
+}
+
+  enter(questionIndex: number, value: number): void {
+    this.hoveredValue[questionIndex] = value;
+  }
+
+  rate(questionIndex: number, value: number): void {
+    this.ratings[questionIndex] = value;
+    this.hoveredValue[questionIndex] = value;
+
+    const formArray = this.questionnaireForm.get('ratings') as FormArray;
+    formArray.controls[questionIndex].setValue(value);
+  }
+
+  submitForm(): void {
+    if (this.questionnaireForm.valid) {
+      // You can access the submitted ratings using this.questionnaireForm.value.ratings
+      console.log('Submitted Ratings:', this.questionnaireForm.value.ratings);
+      this.close()
+    } else {
+      console.log('Form is not valid. Please answer all questions.');
+    }
+  }
 
   close() {
     this.modalCtrl.dismiss();
--- src/app/classes-modal/classes-modal.module.ts
+++ src/app/classes-modal/classes-modal.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +15,8 @@ import { ClassEvaluationModalComponent } from './class-evaluation-modal/class-ev
     CommonModule,
     FormsModule,
     IonicModule,
-    ClassesModalPageRoutingModule
+    ClassesModalPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ClassesModalPage, MessageModalComponent,ClassEvaluationModalComponent]
 })
