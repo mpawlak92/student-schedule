d9d9c8c9959bd943f80a59705c2664a8c9d0f078
Michal Pawlak <mpawlak.it@gmail.com>
2023-12-11 21:03:01 +0100

    added edit profile form

--- src/app/user-news/edit-profile-modal/edit-profile-modal.component.ts
+++ src/app/user-news/edit-profile-modal/edit-profile-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController, NavParams } from '@ionic/angular';
 
 @Component({
@@ -8,18 +9,39 @@ import { ModalController, NavParams } from '@ionic/angular';
 })
 export class EditProfileModalComponent  implements OnInit {
   userData:any | undefined
+  editProfileForm!: FormGroup;
+  
   constructor(
     private modalCtrl: ModalController,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private fb: FormBuilder
   ) {
     this.userData = this.navParams.data;
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.initializeForm();
+  }
 
   close() {
     this.modalCtrl.dismiss();
   }
 
+  
+
+  initializeForm(): void {
+    this.editProfileForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]], // Adjust the minimum length as needed
+    });
+  }
+
+  submitForm(): void {
+    if (this.editProfileForm.valid) {
+      const editedProfile = this.editProfileForm.value;
+      // You can now use the 'editedProfile' variable to update the user's profile.
+      console.log('Edited Profile:', editedProfile);
+    }
+  }
 
 }
--- src/app/user-news/user-news.module.ts
+++ src/app/user-news/user-news.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -19,7 +19,8 @@ import { UserProfileModalComponent } from './user-profile-modal/user-profile-mod
     FormsModule,
     IonicModule,
     UserNewsPageRoutingModule,
-    NewsPagePageModule
+    NewsPagePageModule,
+    ReactiveFormsModule
   ],
   declarations: [UserNewsPage, StudentCardModalComponent,EditProfileModalComponent, UserProfileModalComponent]
 })
